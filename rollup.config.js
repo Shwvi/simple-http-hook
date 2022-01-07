38e4db5f32355fcd77addc69e64bdab1addf12f3
Shwvi <50517848+Shwvi@users.noreply.github.com>
2022-01-07 15:41:00 +0800

    feat: npm config done

--- rollup.config.js
+++ rollup.config.js
@@ -2,21 +2,21 @@ import { terser } from "rollup-plugin-terser";
 import json from "@rollup/plugin-json";
 import rollupTypescript from "rollup-plugin-typescript2";
 export default {
-  input: "src/main.ts",
+  input: "src/index.ts",
   output: [
     {
-      name: "simple_http_hook",
-      file: "dist/bundle.js",
+      name: "simple-http-hook",
+      file: "dist/index.js",
       format: "umd",
     },
     {
-        name: "simple_http_hook",
-        file: "dist/es/bundle.js",
-        format: "es",
-      },
+      name: "simple-http-hook",
+      file: "dist/es/index.js",
+      format: "es",
+    },
     {
       name: "simple_http_hook",
-      file: "dist/bundle.min.js",
+      file: "dist/min/simple-http-hook.min.js",
       format: "umd",
       plugins: [terser()],
     },
