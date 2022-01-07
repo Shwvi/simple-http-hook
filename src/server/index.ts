7056b405eb941ec8f0534c1131f5174dc2e6df73
Shwvi <50517848+Shwvi@users.noreply.github.com>
2022-01-07 19:11:25 +0800

    feat: add cors

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,14 +2,17 @@
 import * as Koa from "koa";
 import * as koaBodyparser from "koa-bodyparser";
 import * as koaRouter from "koa-router";
+import * as koaCors_ from "koa-cors";
 // require will finall import the correct packages
 const koa = require("koa");
 const Router = require("koa-router");
 const bodyparser = require("koa-bodyparser");
+const koaCors = require("koa-cors");
 function createHookServer() {
   const app = new koa();
   const router = new Router();
   app.use(bodyparser({}));
+  app.use(koaCors());
   function registerHook(
     path: string,
     methods: string[],
